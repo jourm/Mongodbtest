0b415f92295cedec3c5eb257cd2e1c1d5aa39e09
jourm <jourm91@gmail.com>
2020-03-26 14:28:34 +0000

    completed pymongo lesson

--- app.py
+++ app.py
@@ -12,17 +12,134 @@ COLLECTION_NAME = "myFirstMDB"
 def mongo_connect(url):
     try:
         conn = pymongo.MongoClient(url)
-        print("Mongo is Conected!")
+
         return conn
     except pymongo.errors.ConnectionFailure as e:
         print("Could not connect to Mongodb: %s") % e
 
 
-conn = mongo_connect(MONGO_URI)
+def show_menu():
+    print("")
+    print("1. Add a record")
+    print("2. Find a record")
+    print("3. Edit a record")
+    print("4. Delete a record")
+    print("5. Exit")
 
+    option = input("Enter option: ")
+    return option
+
+
+def get_record():
+    print("")
+    first = input("Enter first name > ")
+    last = input("Enter last name > ")
+
+    try:
+        doc = coll.find_one({'first': first.lower(), 'last': last.lower()})
+    except:
+        print("Error accesing the database")
+
+    if not doc:
+        print("")
+        print("Error! No results found.")
+
+    return doc
+
+
+def find_record():
+    doc = get_record()
+    if doc:
+        print("")
+        for k, v in doc.items():
+            if k != "_id":
+                print(k.capitalize() + ": " + v.capitalize())
+
+
+def edit_record():
+    doc = get_record()
+    if doc:
+        update_doc = {}
+        print("")
+        for k, v in doc.items():
+            if k != "_id":
+                update_doc[k] = input(k.capitalize() + " [" + v + "] > ")
+
+                if update_doc[k] == "":
+                    update_doc[k] = v
+
+        try:
+            coll.update(doc, {'$set': update_doc})
+            print("")
+            print("Document Uppdated")
+        except:
+            print("Error accesing the database")
+
+
+def delete_record():
+    doc = get_record()
+    if doc:
+        print("")
+        for k, v in doc.items():
+            if k != "_id":
+                print(k.capitalize() + ": " + v.capitalize())
+        print("")
+        confirmation = input(
+            "Is this the document you want to delete?\nY or N > ")
+        print("")
+
+        if confirmation.lower == y:
+            try:
+                coll.remove(doc)
+                print("Document deleted")
+            except:
+                print("Error accesing the database")
+        else:
+            print("Document not deleted")
+
+
+def add_record():
+    print("")
+    first = input("Enter first name > ")
+    last = input("Enter last name > ")
+    dob = input("Enter date of birth > ")
+    gender = input("Enter gender > ")
+    hair_colour = input("Enter hair colour > ")
+    occupation = input("Enter occupation > ")
+    nationality = input("Enter nationality > ")
+
+    new_doc = {'first': first.lower(), 'last': last.lower(), 'dob': dob,
+               'gender': gender, 'hair_colour': hair_colour, 'occupation':
+               occupation, 'nationality': nationality}
+
+    try:
+        coll.insert(new_doc)
+        print("")
+        print("Document Inserted")
+    except:
+        print("Error accesing the database")
+
+
+def main_loop():
+    while True:
+        option = show_menu()
+        if option == "1":
+            add_record()
+        elif option == "2":
+            find_record()
+        elif option == "3":
+            edit_record()
+        elif option == "4":
+            delete_record()
+        elif option == "5":
+            conn.close()
+            break
+        else:
+            print("Invalid option")
+        print("")
+
+
+conn = mongo_connect(MONGO_URI)
 coll = conn[DBS_NAME][COLLECTION_NAME]
 
-documents = coll.find()
-
-for doc in documents:
-    print(doc)
+main_loop()
